ca04ff6f2efba6cd09f8dd4d2338832cf4f1a812
artistrea <apiravesi@gmail.com>
2023-07-25 17:22:28 -0300

    type R control

--- modelsim/control_unit.vhd
+++ modelsim/control_unit.vhd
@@ -19,30 +19,54 @@ entity control_unit is
 end control_unit;
 
 architecture control_unit_arch of control_unit is
-    type instructions is (
-        INS_ADD, INS_ADDi, INS_SUB, INS_SUBi, INS_AND, INS_ANDi, INS_LUI, INS_SLT, INS_OR, INS_ORi,
-        INS_XOR, INS_XORi, INS_SLLi, INS_SRLi, INS_SRAi, INS_SLTi, INS_SLTu, INS_SLTUi, INS_AUIPC,
+    type instruction_enum is (
+        INS_ADD, INS_ADDi, INS_SUB, INS_SUBi, INS_AND, INS_ANDi, INS_LUI, INS_SLT, INS_OR, INS_ORi, INS_XOR, INS_XORi, INS_SLLi, INS_SRLi, INS_SRAi, INS_SLTi, INS_SLTu, INS_SLTUi, INS_AUIPC, INS_JAL, INS_JALR, INS_BEQ, INS_BNE, INS_BLT, INS_BGE, INS_BGEU, INS_BLTU, INS_LW, INS_SW, Unknown_instruction
     );
-    signal format : FORMAT_RV;
+    signal instruction_is : instruction_enum;
 
-
+    signal opcode : std_logic_vector(6 downto 0);
+    signal funct3 : std_logic_vector(2 downto 0);
+    signal funct7 : std_logic_vector(6 downto 0);
+    constant NON_ZERO_FUNCT7 : std_logic_vector(6 downto 0) := "0100000";
 begin
+    opcode <= instr(6 downto 0);
+    funct3 <= instr(14 downto 12);
+    funct7 <= instr(31 downto 25);
+
     process(instr)
     begin
-        case instr(6 downto 0) is
-            when "0110011" => format <= R_type;
-            when "0000011" => format <= I_type;
-            when "0010011" => format <= I_type;
-            when "1100111" => format <= I_type;
-            when "0100011" => format <= S_type;
-            when "1100011" => format <= SB_type;
-            when "0110111" => format <= U_type;
-            when "1101111" => format <= UJ_type;
-            when others => format <= Unknown_type;
+        case opcode is
+            when "0110011" => -- format <= R_type;
+                if funct7 = NON_ZERO_FUNCT7 and funct3 = "000" then
+                    instruction_is <= INS_SUB;
+                elsif funct7 = NON_ZERO_FUNCT7 then
+                    instruction_is <= Unknown_instruction; -- SRA não especificado no trabalho
+                else
+                    case funct3 is
+                        when "000" => instruction_is <= INS_ADD;
+                        -- when "001" => instruction_is <= INS_SLL;
+                        when "010" => instruction_is <= INS_SLT;
+                        when "011" => instruction_is <= INS_SLTU;
+                        when "100" => instruction_is <= INS_XOR;
+                        -- when "101" => instruction_is <= INS_SRL;
+                        when "110" => instruction_is <= INS_OR;
+                        when "111" => instruction_is <= INS_AND;
+                        when others => instruction_is <= Unknown_instruction;
+                    end case;
+                end if;
+            -- when "0000011" => format <= I_type;
+            -- when "0010011" => format <= I_type;
+            -- when "1100111" => format <= I_type;
+            when "0100011" => instruction_is <= INS_ADDi;
+            when "1100011" => instruction_is <= INS_ADDi;
+            when "0110111" => instruction_is <= INS_ADDi;
+            when "0010111" => instruction_is <= INS_ADDi;
+            when "1101111" => instruction_is <= INS_ADDi;
+            when others => instruction_is <= Unknown_instruction;
         end case;
     end process;
 
-    process(format, instr)
+    process(instruction_is, instr)
     begin
             
     end process;
