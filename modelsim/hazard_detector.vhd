364f8efa36204cf951c20e24a04334d6f749b2ff
artistrea <apiravesi@gmail.com>
2023-07-25 16:18:37 -0300

    added hazard detectror

---
+++ modelsim/hazard_detector.vhd
@@ -0,0 +1,16 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity hazard_detector is
+    port (
+        a : in std_logic_vector(31 downto 0)
+    );
+end hazard_detector;
+
+architecture hazard_detector_arch of hazard_detector is
+
+begin
+end hazard_detector_arch;
+
